859b38398996de2bd6dff6b8a8c69051a21e9580
[REM-FR]Arnaud Didier <84334523+nono-web@users.noreply.github.com>
2023-01-19 11:36:46 +0100

    Title

--- react-base/src/App.js
+++ react-base/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
- 
+ <Title name='Arnaud'/>
     </div>
   );
 }
---
+++ react-base/src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title = ({name}) => {
+  return (
+    <div>
+      <h1>Bonjour {name}</h1>
+    </div>
+  )
+}
+
+export default Title
+
